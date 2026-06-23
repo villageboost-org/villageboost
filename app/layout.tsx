dd47535d9f6369736091634181198f3555e9d56f
duncan-chege <dshege4@gmail.com>
2026-06-23 22:22:33 +0300

    change the main font to figtree

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
+
+const figtree = Figtree({
+  subsets: ["latin"],
+  variable: "--font-figtree",
+});
 
 export const metadata: Metadata = {
   title: "Village Boost",
@@ -14,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full scroll-smooth">
+    <html lang="en" className={`${figtree.variable} h-full scroll-smooth`}>
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
