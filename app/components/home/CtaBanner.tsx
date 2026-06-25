28eda95c7c76935acbe34fdb7883f5f5d39ed25d
duncan-chege <dshege4@gmail.com>
2026-06-25 09:20:29 +0300

    change banner image to webp format

--- app/components/home/CtaBanner.tsx
+++ app/components/home/CtaBanner.tsx
@@ -2,7 +2,7 @@ export default function CtaBanner() {
   return (
     <section
       className="relative bg-cover bg-center bg-fixed bg-no-repeat px-8 py-44 text-center text-white"
-      style={{ backgroundImage: "url('/banner-bg.jpg')" }}
+      style={{ backgroundImage: "url('/banner-bg.webp')" }}
     >
       <div className="relative">
         <h2 className="text-white">Include your village in your next project</h2>
